b67e6a746f3157103571c66a8ae74380b952cf40
Traiitor <143916604+Traiitor@users.noreply.github.com>
2024-06-05 14:22:51 +0800

    Update MarkovPrefetcher.scala

--- src/main/scala/prefetcher/MarkovPrefetcher.scala
+++ src/main/scala/prefetcher/MarkovPrefetcher.scala
@@ -9,9 +9,25 @@ class MarkovPrefetcher extends Module {
   val io = IO(new MarkovPrefetcherIO)
 
   // Implement your Markov prefetcher logic here
-  io.prefetch := DontCare
-  io.prefetchAddr := DontCare
-}
+  val tableSize = 256 // Size of the history table
+  val historyTable = RegInit(VecInit(Seq.fill(tableSize)(false.B)))
+  val patternHistory = RegInit(0.U(tableSize.W))
+
+  val prediction = historyTable(patternHistory)
+
+  val miss = !io.hit
+
+  // Update history table
+  historyTable(patternHistory) := miss
+
+  // Update pattern history
+  patternHistory := Cat(patternHistory, miss).asUInt
+
+  // Prefetch based on prediction
+  val prefetchAddr = io.addr + Mux(prediction, io.stride, 0.U)
+
+  io.prefetch := prediction
+  io.prefetchAddr := prefetchAddr
 
 object MarkovPrefetcherMain extends App {
   ChiselStage.emitSystemVerilogFile(new MarkovPrefetcher)
